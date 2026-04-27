dc93daf4709ac99fd9eda225766d0ec981e53256
mzeejosh01 <89415222+mzeejosh01@users.noreply.github.com>
2026-04-27 18:42:00 +0200

    update

--- server/app.py
+++ server/app.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_socketio import SocketIO
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../client", static_url_path="")
 app.config['SECRET_KEY'] = 'secret!'
 
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route('/')
-def home():
-    return "Server is running"
+def serve_index():
+    return send_from_directory(app.static_folder, "index.html")
 
 @socketio.on('connect')
 def handle_connect():
@@ -17,6 +17,3 @@ def handle_connect():
 @socketio.on('disconnect')
 def handle_disconnect():
     print("A user disconnected!")
-
-if __name__ == '__main__':
-    socketio.run(app, debug=True)
